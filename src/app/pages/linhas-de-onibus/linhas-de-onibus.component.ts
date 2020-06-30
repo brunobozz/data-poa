43199df78cc2a06686ccaa46ec48a7144eae239d
Bruno Tourn Bozzetti <bruno.bozzetti@dbserver.com.br>
2020-06-30 19:57:11 -0300

    pages and routes are created

---
+++ src/app/pages/linhas-de-onibus/linhas-de-onibus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linhas-de-onibus',
+  templateUrl: './linhas-de-onibus.component.html',
+  styleUrls: ['./linhas-de-onibus.component.scss']
+})
+export class LinhasDeOnibusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
