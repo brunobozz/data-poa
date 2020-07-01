2414ac35c7f4fc588e0b1fd001be35fa7f01737b
Bruno Tourn Bozzetti <bruno.bozzetti@dbserver.com.br>
2020-06-30 23:35:04 -0300

    api service is created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+
+// COMPONENTS
 import { HeaderComponent } from './components/header/header.component';
 import { ContentComponent } from './components/content/content.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+// PAGES
 import { LinhasDeOnibusComponent } from './pages/linhas-de-onibus/linhas-de-onibus.component';
 import { LinhasDeLotacaoComponent } from './pages/linhas-de-lotacao/linhas-de-lotacao.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Er404Component } from './pages/er404/er404.component';
+
+// SERVICES
+import { ApiDadosabertosModule } from './modules/api-dadosabertos/api-dadosabertos.module';
 
 @NgModule({
   declarations: [
@@ -24,7 +31,9 @@ import { Er404Component } from './pages/er404/er404.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApiDadosabertosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
